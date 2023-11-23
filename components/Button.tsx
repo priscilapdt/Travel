7e8c8f38c51077e9cbfbe89951dfb486642ede4d
Priscila Pedrotti <priscila.pedrotti@outlook.com>
2023-11-23 15:28:24 -0300

    add getApp page

--- components/Button.tsx
+++ components/Button.tsx
@@ -4,16 +4,17 @@ type ButtinProps = {
   type: "button" | "submit";
   title: string;
   icon?: string;
-  variant: "btn_dark_green";
+  variant:string;
+  full?:boolean;
 };
-const Button = ({ type, title, icon, variant }: ButtinProps) => {
+const Button = ({ type, title, icon, variant , full}: ButtinProps) => {
   return (
     <button
       type={type}
-      className={`flexCenter gap-3 rounded-full border ${variant}`}
+      className={`flexCenter gap-3 rounded-full border ${variant} ${full && 'w-full'}`}
     >
       {icon && <Image src={icon} alt={title} width={24} height={24} />}
-      <label className="bold-16 whitespace-nowrap">{title}</label>
+      <label className="bold-16 whitespace-nowrap cursor-pointer">{title}</label>
     </button>
   );
 };
