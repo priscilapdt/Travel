8f4ef2787e028dab733363517b0d70f7473f248d
Priscila Pedrotti <priscila.pedrotti@outlook.com>
2023-11-23 10:05:13 -0300

    add new components and Hero page

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,17 @@
+import Hero from "@/components/Hero";
+import Camp from "@/components/Camp";
+import Guide from "@/components/Guide";
+import Features from "@/components/Features";
+import GetApp from "@/components/GetApp";
 
 export default function Home() {
   return (
-   <h1 className='font-bold text-teal-500 text-2xl'>hello</h1>
-  )
+    <>
+      <Hero />
+      <Camp />
+      <Guide />
+      <Features />
+      <GetApp />
+    </>
+  );
 }
